b71d5aee4501fdd51ff003848ac529e2b140a0e4
Avatardomal <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>
2025-08-26 19:20:53 -0300

    Projeto BFD - OFC

--- src/main.js
+++ src/main.js
@@ -23,7 +23,19 @@ async function perguntarIdade(){
     for (let k in pessoa) {
         console.log(`${k} + ${pessoa[k]}`)
     }
+
+    
+    const array1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    // Pass a function to map
+    const map = array1.reverse();
+
+    console.log(map);
+    // Expected output: Array [2, 8, 18, 32]
+    
     rl.close();
+
+
 }
 
 perguntarIdade();
