39388ad7f1ed0dbc585f609bbb2a51d33a3952f4
Avatardomal <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>
2025-08-26 19:51:57 -0300

    Calculadora de média

--- src/main.js
+++ src/main.js
@@ -1,46 +1,52 @@
-const readline = require('readline');
-const rl = readline.createInterface({
+const readline = require("readline");
+const notas = readline.createInterface({
     input: process.stdin,
-    output: process.stdout      
-});    
+    output: process.stdout
+});
 
-function nota(pergunta) {
-    return new Promise((resolve) => {
-        rl.question(pergunta, (resposta) => {
-            resolve(Number(resposta));
+//Fução que faz a pergunta e retorna uma Promise
+function pergunta(questao) {
+    return new Promise ((resolve) => {
+        notas.question((questao), (resposta) => {
+            resolve(resposta);
         });
     });
+};
 
-}
 
-async function perguntarIdade(){
-    let pessoa = {
-        nome: 'Jean',
-        idade: 40,
-        cidade: "Recife" 
-    }
-  
-    for (let k in pessoa) {
-        console.log(`${k} + ${pessoa[k]}`)
-    }
+async function inputNotas() {
+    const nota1 = await pergunta('Digite sua primeira nota: ');
+    const nota2 = await pergunta('Digite sua segunda nota: ');
 
+    const n1 = parseFloat(nota1);
+    const n2 = parseFloat(nota2);
+
+    if (n1 > 10 || n2 > 10 || n1 < 0 || n2 < 0 || isNaN(n1) || isNaN(n2)) {
+        console.log();
+        console.log('=== Notas digitadas de maneira inválida! O sistema não aceitará textos e as notas precisam ser números entre 0 e 10! ===');
+        console.log('=== Digite suas notas novamente: ===');
+        console.log();
+        return inputNotas();
+    } else {
+        calcMedia(n1, n2);
+    };
+};
+
+function calcMedia(n1, n2) {
     
-    const array1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    
+    let media = (n1 + n2) / 2;
+    console.log();
+    console.log(`== Nota média = ${media} ==`);
 
-    // Pass a function to map
-    const map = array1.reverse();
+    if (media >= 7) {
+         console.log('== Situação: Aprovado(a). ==');
+         console.log();
+     } else {
+        console.log('== Situação: Reprovado(a). ==');
+        console.log();
+    };
+    notas.close();
+};
 
-    console.log(map);
-    // Expected output: Array [2, 8, 18, 32]
-
-    for (let i = 0; i < map.length; i++) {
-  console.log(map[i]);
-}
-
-
-    rl.close();
-
-
-}
-
-perguntarIdade();
+ inputNotas();
