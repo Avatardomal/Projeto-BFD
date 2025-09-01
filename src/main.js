361ddc6ef9dede4d35956e796eecebf661738c0d
Avatardomal <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>
2025-09-01 17:21:20 -0300

    Regras Variaveis

--- src/main.js
+++ src/main.js
@@ -1,4 +1,40 @@
-console.log('Meu nome é "Jean Celerino". Estou aprendendo JavaScript às', 10, 'da manhã.');
+// Sempre criar variáveis com nomes significativos
+//let nome; // declaração de variável
+//nome - 'Qualquer valor'; // Incialização de variável
+//nome = 'Jean Celerino'; // atribuição de valor //
+//console.log(nome);
+//nome = 'Erica';
+//console.log(nome);
+//Não podemos redeclarar variaveis com let
+//let nome = 'Erica'; // ERRO
+// Não podemos criar variáveis com palavras reservadas
+// let let = 'Jean'; // ERRO
+// let if = 'Jean'; // ERRO
+// variaveis precisam ter nomes significativos
+// Evitar nomes genéricos e siglas
+// let n = 'Jean'; // Não é bom
+// Não pode começar o nome de uma variável com número
+// let 1nome = 'Jean'; // ERRO
+// let nome1 = 'Jean'; // Certo
+// Não podem conter espaços ou traços
+// let nome-completo = 'Jean Celerino'; // ERRO
+// let nome completo = 'Jean Celerino'; // ERRO
+// ultilizampos o camelCase
+// let nomeCompleto = 'Jean Celerino'; // Certo
+// Case-sensitive
+// let nome = 'Jean'; // Criou a variável nome
+
+let nome = 'Jean Celerino';
+let nomeEsposa = 'Erica';
+let nomeFilha = 'Eva'; 
+
+console.log(nome,'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu', nomeEsposa, '.');
+console.log(nome, 'casou-se com', nomeEsposa, 'em 2012.');
+console.log(nomeEsposa, 'teve 1 filho com', nome, 'em 2015.');
+console.log('A filha de', nome, 'se chama', nomeFilha, '.');
+
+
 
 
 
