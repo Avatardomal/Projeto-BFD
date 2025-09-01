45b0dafa85b636bc5ed226c3d9605b869023f628
Avatardomal <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>
2025-09-01 16:51:57 -0300

    console.log2

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-console.log("Olá, Mundo!"); //String = Texto
-console.log(`"Este é o meu primeiro código" em 'JavaScript!'`);
+console.log('Meu nome é "Jean Celerino". Estou aprendendo JavaScript às', 10, 'da manhã.');
 
-console.log(123456); //Number = Número
-console.log(12.345); //Number = Número com ponto flutuante
-console.log(123 + 456); //Number = Número com operação matemática
-console.log(40,15.85, 'Testanto vírgula'); //Number = Número com operação matemática
-console.log(123456, "Testanto vírgula", 12.345); //Number = Número com operação matemática
+
 
 
 
