002077bc6dd8589692bbb8502533e2bb71b74035
Avatardomal <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>
2025-09-01 22:44:31 -0300

    Const

--- src/main.js
+++ src/main.js
@@ -1,41 +1,37 @@
-// Sempre criar variáveis com nomes significativos
-//let nome; // declaração de variável
-//nome - 'Qualquer valor'; // Incialização de variável
-//nome = 'Jean Celerino'; // atribuição de valor //
-//console.log(nome);
-//nome = 'Erica';
-//console.log(nome);
-//Não podemos redeclarar variaveis com let
-//let nome = 'Erica'; // ERRO
-// Não podemos criar variáveis com palavras reservadas
-// let let = 'Jean'; // ERRO
-// let if = 'Jean'; // ERRO
-// variaveis precisam ter nomes significativos
-// Evitar nomes genéricos e siglas
-// let n = 'Jean'; // Não é bom
-// Não pode começar o nome de uma variável com número
-// let 1nome = 'Jean'; // ERRO
-// let nome1 = 'Jean'; // Certo
+// Não podemos criar constantes com plavras reservadas
+// Exemplos de palavras reservadas: if, else, for, while, do, break, true, false, function, return, var, let, const
+// Constantes precisam ter nomes significativos
 // Não podem conter espaços ou traços
-// let nome-completo = 'Jean Celerino'; // ERRO
-// let nome completo = 'Jean Celerino'; // ERRO
-// ultilizampos o camelCase
-// let nomeCompleto = 'Jean Celerino'; // Certo
-// Case-sensitive
-// let nome = 'Jean'; // Criou a variável nome
-// Não utilize var, utilize let (ES6) ou const (ES6)
-// var nome = 'Jean'; // Não ultilizar
-// let nome = 'Jean'; // Ultilizar
+// Utilizamos camelCase para nomear nossas constantes
+// case sensitive - letras maiúsculas e minúsculas fazem diferença
+// Não podemos redeclarar uma constante
+// Não podemos alterar o valor de uma constante
+// Não utilize var, utilize const (ou let)
+// + - * /
+// String = Texto "Assim" 'Assim' `Assim`
+// Number = 1, 2, 3, 4, 5...
+// Boolean = true, false
 
-let nome = 'Jean Celerino';
-let nomeEsposa = 'Erica';
-let nomeFilha = 'Eva'; 
+const primeiroNumero = 5;  //Number tipo de dado
+const segundoNumero = 10;  
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
 
-console.log(nome,'nasceu em 1984.');
-console.log('Em 2000', nome, 'conheceu', nomeEsposa, '.');
-console.log(nome, 'casou-se com', nomeEsposa, 'em 2012.');
-console.log(nomeEsposa, 'teve 1 filho com', nome, 'em 2015.');
-console.log('A filha de', nome, 'se chama', nomeFilha, '.');
+console.log(typeof primeiroNumero);
+console.log(primeiroNumero + segundoNumero);
+console.log(resultado);
+console.log(resultadoDuplicado);
+console.log(resultadoTriplicado);
+console.log(typeof resultadoTriplicado);
+
+const minhaString = "Minha String";
+
+
+
+
+
 
 
 
