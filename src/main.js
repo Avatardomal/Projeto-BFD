3f0a00272eb2a848df2f078cfe578d7011aa96d0
Avatardomal <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>
2025-09-02 10:34:32 -0300

    Operadores Aritimeticos

--- src/main.js
+++ src/main.js
@@ -1,20 +1,68 @@
-// ECMAScript 6 (ES6) features
-// Arrow functions, let and const, template literals, destructuring, modules, classes, promises
+/* 
+- Aritimetica Simples + - / * %
+ * + Adição / Concatenação EX:
+ * console.log(2 + 2);
+ * console.log("2" + "2");
+ * console.log("2" + 2);
+ * console.log(2 + "2");
+ 
+    * - Subtração EX
+    * console.log(2 - 2);
+    * console.log("2" - "2");
+    * console.log("2" - 2);
+    * console.log(2 - "2");
 
-//nome = 'Jean'; // Variável global (não faça isso)
+    * / Divisão EX  
+    * console.log(2 / 2);
+    * console.log("2" / "2");
+    * console.log("2" / 2);
+    * console.log(2 / "2");
+    * * * Multiplicação EX
+    * console.log(2 * 2);
+    * console.log("2" * "2");
+    * console.log("2" * 2);
+    * console.log(2 * "2");
+    * % Resto da Divisão EX
+    * console.log(11 % 2);
+    * console.log(10 % 2);
+*/
 
-// variaveis primitivas: string, number, boolean, undefined, null (bigint, symbol)
+/* Ordem de Precedência
+ * 1 = ()
+ * 2 = **
+ * 2 = * / %
+ * 3 = + -
+*/
 
-const nome = 'Jean'; // String
-const nome1 = "Philipe";// String
-const nome2 = `Celerino`; // String 
-const num1 = 10; // number 
-const num2 = 10.52; // number 
-let nomeAluno; // undefined => não aponta para lugar nenhum na memória 
-const sobreNomeAluno = null; // Nulo => não aponta para lugar nenhum na memória
-const aprovado = false; // boolean = true, false (lógico)
+//const num1 = 10;
+//const num2 = 5;
+//const num3 = 2;
+//console.log(num1 + num2 * num3); // Ordem de precedência
+//console.log((num1 + num2) * num3); // Ordem de precedência com parênteses
 
+// Incremento ++ Decremento --
+//let contador = 1;
+//contador++; // 2;
 
+/*
+const passo = 50; 
+let contador = 0;
+contador++; 
 
+contador += passo; 
+contador += passo; 
+contador += passo; // contador = contador + passo
+console.log(contador);
+*/
 
+//NaN - Not a Number
+//console.log(100 / "Maçã");
+//console.log(100 * "Maçã");
+//console.log(100 - "Maçã");
+
+let contador = 2;   
+contador *= 2; // contador = contador * 2   
+contador *= 2; // contador = contador * 2
+contador *= 2; // contador = contador * 2
+console.log(contador);
 
