93e3f2a1487373635370a28b8beb986b5b393594
Avatardomal <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>
2025-09-02 15:31:06 -0300

    Mais praticas de stings

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
-let varA = 'A'; // B
-let varB = 'B'; // C
-let varC = 'C'; // A
+//               0123456789
 
-/* Opção de resolução 1
-const varATemp = varA;
-varA = varB;
-varB = varC;
-varC = varATemp;
-*/
+let umaString = "Um texto";
 
-[varA, varB, varC] = [varB, varC, varA]; // Opção de resolução 2 - Destructuring
+console.log(umaString.concat(' ', 'em', ' ', 'um', ' ',  'lindo', ' ', 'dia')); // Retorna o caractere que está na posição 3 da string
+console.log(umaString + ' em um lindo dia'); // Retorna o caractere que está na posição 3 da string
+console.log(`${umaString} em um lindo dia`); // Retorna o caractere que está na posição 3 da string
+console.log(umaString.indexOf('Um texto')); // Retorna o índice do caractere "t" na string
+console.log(umaString.lastIndexOf('m')); // Retorna o índice do caractere "m" na string
+console.log(umaString.match(/[a-z]/g)); // Retorna um array com as letras de "a" a "z"
+console.log(umaString.match(/[a-z]/)); // Retorna um array com a primeira letra de "a" a "z"
+console.log(umaString.search(/x/)); // Retorna o índice do caractere "x" na string
+console.log(umaString.replace('Um', 'Outro')); // Substitui a palavra "Um" por "Outro"
+console.log(umaString.length); // Retorna o tamanho da string
+console.log(umaString.slice(3, 7)); // Retorna os caracteres entre os índices 3 e 7
+console.log(umaString.slice(-3)); // Retorna os últimos 3 caracteres
+console.log(umaString.split(' ')); // Retorna um array quebrando a string pelo espaço
+console.log(umaString.toUpperCase()); // Retorna a string em letras maiúsculas
+console.log(umaString.toLowerCase()); // Retorna a string em letras minúsculas
+console.log(umaString.trim()); // Retorna a string sem espaços no início e no fim da string
+console.log(umaString.split('')); // Retorna um array quebrando a string por cada caractere
 
-console.log(varA, varB, varC);
