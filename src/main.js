19e5cf15c0be2b3c772969504c479c8a43eb3235
Avatardomal <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>
2025-09-01 23:12:03 -0300

    Const e let

--- src/main.js
+++ src/main.js
@@ -1,36 +1,30 @@
-// Não podemos criar constantes com plavras reservadas
-// Exemplos de palavras reservadas: if, else, for, while, do, break, true, false, function, return, var, let, const
-// Constantes precisam ter nomes significativos
-// Não podem conter espaços ou traços
-// Utilizamos camelCase para nomear nossas constantes
-// case sensitive - letras maiúsculas e minúsculas fazem diferença
-// Não podemos redeclarar uma constante
-// Não podemos alterar o valor de uma constante
-// Não utilize var, utilize const (ou let)
-// + - * /
-// String = Texto "Assim" 'Assim' `Assim`
-// Number = 1, 2, 3, 4, 5...
-// Boolean = true, false
-
-const primeiroNumero = 5;  //Number tipo de dado
-const segundoNumero = 10;  
-const resultado = primeiroNumero * segundoNumero;
-const resultadoDuplicado = resultado * 2;
-let resultadoTriplicado = resultado * 3;
-resultadoTriplicado = resultadoTriplicado + 5;
-
-console.log(typeof primeiroNumero);
-console.log(primeiroNumero + segundoNumero);
-console.log(resultado);
-console.log(resultadoDuplicado);
-console.log(resultadoTriplicado);
-console.log(typeof resultadoTriplicado);
-
-const minhaString = "Minha String";
+/*
+Jean Celerino tem 40 anos, pesa 138 kg
+tem 1.80 de altura e seu IMC é de 42.59
+Jean nasceu em 1985
+*/
 
 
+const nome = "Jean";
+const sobrenome = "Celerino";
+const idade = 40;
+const peso = 138;
+const alturaEmM = 1.80;
+let indiceMassaCorporal;//Peso /(altura * altura)
+indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
+let anoNascimento;
+anoNascimento = 2025- idade;
+console.log(nome, sobrenome, "tem", idade, "anos, pesa", peso, "kg");
+console.log("tem", alturaEmM, "de altura e seu Indice de Massa Corporal é de", indiceMassaCorporal);
+console.log(nome, "nasceu em", anoNascimento);
 
 
+/* Outro modo de escrever
+template strings ou template literals
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`);
+console.log(`${nome} nasceu em ${anoNascimento}`);
+*/
 
 
 
