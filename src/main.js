b34d83b366c835834c23394a0a0a6f7d393431d8
Avatardomal <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>
2025-09-06 13:41:10 -0300

    number

--- src/main.js
+++ src/main.js
@@ -1,47 +1,13 @@
-// 1. Classe Carro
-class Carro {
-  constructor(placa, cor) {
-    this.placa = placa;
-    this.cor = cor;
-  }
+let mum1 = 10;
+let mum2 = 2.5;
+/*
+console.log(num1.toString() + num2);
+num1 = num1.toString();
+console.log(num1 + num2(2));
+console.log(num1.toString(4));
+*/
+console.log(Number.isInteger(mum1)); //Indentifiando se o numero é inteiro  
+console.log(Number.isInteger(mum2));//Indentifiando se o numero é inteiro
 
-  // Método para exibir informações do carro
-  mostrarInfo() {
-    return `Carro - Placa: ${this.placa}, Cor: ${this.cor}`;
-  }
-}
-
-// 2. Classe Estacionamento
-class Estacionamento {
-  constructor() {
-    this.vagas = []; // array vazio
-  }
-
-  // Adicionar carro
-  adicionarCarro(carro) {
-    this.vagas.push(carro);
-    console.log(`Carro com placa ${carro.placa} estacionado.`);
-  }
-
-  // Listar carros estacionados
-  listarCarros() {
-    console.log("=== Carros estacionados ===");
-    this.vagas.forEach((carro, index) => {
-      console.log(`${index + 1}. ${carro.mostrarInfo()}`);
-    });
-  }
-}
-
-// 3. Criar 3 objetos Carro
-const carro1 = new Carro("ABC-1234", "Vermelho");
-const carro2 = new Carro("XYZ-5678", "Preto");
-const carro3 = new Carro("JKL-9101", "Branco");
-
-// 4. Criar um Estacionamento e adicionar os carros
-const estacionamento = new Estacionamento();
-estacionamento.adicionarCarro(carro1);
-estacionamento.adicionarCarro(carro2);
-estacionamento.adicionarCarro(carro3);
-
-// 5. Listar carros no console
-estacionamento.listarCarros();
+let temp = mum1 * 'ola';
+console.log(Number.isNaN(temp)); //Indentificando se o valor é NaN (Not a Number) 
